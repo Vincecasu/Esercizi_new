e9f073f1a988ccfbbf23a76df9ac6c483b9b3594
Vincecasu <vincenzocasuccio77@gmail.com>
2024-04-18 19:49:03 +0200

    Aggiunto solo l'argomento dell' esercizio da fare

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -23,3 +23,10 @@ name_title: str = name.title()
 
 print(f"{name_lower}, {name_upper}, {name_title}")
 
+#2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+#Your output should look something like the following, including the quotation marks: Albert Einstein once said,
+#“A person who never made a mistake never tried anything new.”
+
+
+
+
