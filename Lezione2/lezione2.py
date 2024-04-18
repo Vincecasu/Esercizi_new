7bf726bbf56561b40330adcc43066a34b190a72e
Vincecasu <vincenzocasuccio77@gmail.com>
2024-04-18 19:18:04 +0200

    Risolto esercizio 1

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -1,4 +1,13 @@
 # Vincenzo Casuccio
 # 18/04/2024
 
-print ("Hello World!")
+print("Hello World!")
+
+#2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+# Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+name : str = "Mario"
+
+message : str = (f"Ciao {name}, ti va di imparare un pò di python insieme")
+
+print(message)
