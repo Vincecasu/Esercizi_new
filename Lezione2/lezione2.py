3dff38b87b460bb122bb3196aabd6746c1997121
vincecasu <vincenzocasuccio77@gmail.com>
2024-04-27 19:42:24 +0200

    Esercizi_Lezione2fino3.7

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -63,7 +63,7 @@ for s in names:
 
 moto: list = ["Ducati","Bmw","Aprilia"]
 statements: list = ["Adoro girare con la mia","Mi piacerebbe provare una",\
-                    "Non ho mai guidato una"]
+                    "Non ho mai guidato un'"]
 for i in range(len(statements)):
  print(f"{statements[i]} {moto[i]}")
 
@@ -71,6 +71,86 @@ for i in range(len(statements)):
 #who would you invite? Make a list that includes at least three people you’d 
 #like to invite to dinner. Then use your list to print a message to each person, 
 #inviting them to dinner.
+
+guest_list: list = ["Pippo","Pluto","Topolino"]
+message: str = ", ti va di venire a cena?"
+for m in range(len(guest_list)):
+ print(f"Ciao {guest_list[m]}{message}")
+
+#3-5. Changing Guest List: You just heard that one of your guests can’t make 
+#the dinner, so you need to send out a new set of invitations. You’ll have to 
+#think of someone else to invite.
+#Start with your program from Exercise 3-4. Add a print() call at the end of 
+#your program, stating the name of the guest who can’t make it.
+#Modify your list, replacing the name of the guest who can’t make it with 
+#the name of the new person you are inviting.
+#Print a second set of invitation messages, one for each person who is still 
+#in your list.
+print(f"Purtroppo {guest_list[-1]} non sarà dei nostri..")
+guest_list.pop(-1)
+print(f"{guest_list}")
+guest_list.append("Paperino")
+print(f"{guest_list}")
+for invito in range(len(guest_list)):
+ print(f"Ciao caro {guest_list[invito]}{message}")
+
+#3-6. More Guests: You just found a bigger dinner table, so now more space is 
+#available. Think of three more guests to invite to dinner.
+#Start with your program from Exercise 3-4 or 3-5. Add a print() call to the 
+#end of your program, informing people that you found a bigger table.
+#Use insert() to add one new guest to the beginning of your list.
+#Use insert() to add one new guest to the middle of your list.
+#Use append() to add one new guest to the end of your list.
+#Print a new set of invitation messages, one for each person in your list.
+print(f"Allora guys, abbiamo a disposizione un tavolo piu grande,\
+saremo in sette.")
+guest_list.insert(0,"Aldo")
+print(guest_list)
+guest_list.insert(3,"Giovanni")
+guest_list.append("Giacomo")
+print(guest_list)
+messaggio: str = "ti aspetto per cena mi raccomando"
+for invite in range(len(guest_list)):
+ print(f"{guest_list[invite]} {messaggio}.")
+
+#3-7. Shrinking Guest List: You just found out that your new dinner table 
+#won’t arrive in time for the dinner, and now you have space for only two guests.
+#Start with your program from Exercise 3-6. Add a new line that prints a message 
+#saying that you can invite only two people for dinner.
+#Use pop() to remove guests from your list one at a time until only two names 
+#remain in your list. Each time you pop a name from your list, print a message 
+#to that person letting them know you’re sorry you can’t invite them to dinner.
+#Print a message to each of the two people still on your list, letting them know 
+#they’re still invited.
+#Use del to remove the last two names from your list, so you have an empty list.
+#Print your list to make sure you actually have an empty list at the end 
+#of your program.
+print(f"Spiacente ragazzi ma il tavolo nuovo non arriverà, per cui \
+saremo solo in 3.")
+print(f"Mi spiace {guest_list[-1]}, sarà per la prossima volta. Saluti.")
+guest_list.pop(5)
+print(f"Mi spiace {guest_list[-1]}, sarà per la prossima volta. Saluti.")
+guest_list.pop(4)
+print(f"Mi spiace {guest_list[-1]}, sarà per la prossima volta. Saluti.")
+guest_list.pop(3)
+print(f"Mi spiace {guest_list[-1]}, sarà per la prossima volta. Saluti.")
+guest_list.pop(2)
+print(f"Mi spiace {guest_list[-1]}, sarà per la prossima volta. Saluti.")
+print(guest_list)
+new_mess: str = "ci si vede questa sera!Ciao."
+for last in range(len(guest_list)):
+ print(f"Ok {guest_list[last]} {new_mess}")
+del guest_list [:]
+print(guest_list) 
+
+
+
+
+
+
+
+ 
+
 
 
 
