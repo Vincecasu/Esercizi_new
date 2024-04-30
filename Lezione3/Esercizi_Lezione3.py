68cc5d16f8b59176e14bdbe20669aba2bc35ed58
vincecasu <vincenzocasuccio77@gmail.com>
2024-04-30 14:14:03 +0200

    Esercizi3 quasi finiti

--- Lezione3/Esercizi_Lezione3.py
+++ Lezione3/Esercizi_Lezione3.py
@@ -387,4 +387,114 @@ elif age < 65:
 else:
     print("The person is an elder.")
 
+'''
+5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a 
+series of independent if statements that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit 
+is in your list. If the fruit is in your list, the if block should print a 
+statement, such as You really like Apples!
+'''
+fruits: list[str] = ["mela","pera","arancia","banana","mandarino"]
+if "mela" in fruits:
+    print(f"La mela fa parte della tua lista. ")
+if "pera" in fruits:
+    print(f"La pera è nella lista.")
+if "mango" not in fruits:
+    print("Il Mango fa parte della lista?:")
+    print("mango" in fruits)
 
+
+favorite_fruits: list [str] = ["mela","pera","banana"]
+if "mela" in favorite_fruits:
+    print("You really like apples!")
+if "arancia" not in favorite_fruits:
+    print("L' arancia fa parte dei tuoi frutti preferiti?:")
+    print("arancia" in favorite_fruits)
+
+'''
+5-8. Hello Admin: Make a list of five or more usernames, including the name 
+'admin'. Imagine you are writing code that will print a greeting to each user 
+after they log in to a website. Loop through the list, and print a greeting 
+to each user.
+If the username is 'admin', print a special greeting, such as Hello admin, 
+would you like to see a status report?
+Otherwise, print a generic greeting, such as Hello Jaden, thank you for 
+logging in again.
+'''
+users: list[str] = ["Gino","Nino","Pino","admin","Leo"]
+for i in users:
+    if i == "admin":
+        print(f"Ciao {i}, ti piacerebbe vedere uno status report?")
+    else:
+        print(f"Ciao {i}, grazie per il tuo nuovo accesso.")
+
+'''
+5-9. No Users: Add an if test to hello_admin.py to make sure the list of users 
+is not empty.
+If the list is empty, print the message We need to find some users!
+Remove all of the usernames from your list, and make sure the correct message 
+is printed.
+'''
+users: list[str] = ["Gino","Nino","Pino","admin","Leo"]
+if len(users)> 0:
+    for i in users:
+        if i == "admin":
+            print(f"Ciao {i}, ti piacerebbe vedere uno status report?")
+        else:
+            print(f"Ciao {i}, grazie per il tuo nuovo accesso.")
+else:
+    del users [:]
+print("We need to find some users!")
+
+'''
+5-10. Checking Usernames: Do the following to create a program that simulates 
+how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+• Make another list of five usernames called new_users. Make sure one or two 
+of the new usernames are also in the current_users list.
+• Loop through the new_users list to see if each new username has already 
+been used. If it has, print a message that the person will need to enter 
+a new username. If a username has not been used, print a message saying that 
+the username is available.
+• Make sure your comparison is case insensitive. If 'John' has been used, 
+'JOHN' should not be accepted. (To do this, you’ll need to make a copy of 
+current_users containing the lowercase versions of all existing users.)
+'''
+
+current_users: list[str] = ["Gino","Nino","Pino","Lino","Teo"]
+new_users: list[str] = ["gino","Edo","nino","Manu","pino"]
+for user in new_users:
+    if user in current_users:
+        print(f"Spiacenti {user},la tua username non è disponibile.")
+    else:
+        print(f"Ok {user}, la tua username è disponibile.")
+
+'''
+ 5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, 
+ such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+• Store the numbers 1 through 9 in a list.
+• Loop through the list.
+• Use an if-elif-else chain inside the loop to print the proper ordinal ending 
+for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", 
+and each result should be on a separate line.  
+'''
+numeri_ordinali: list[int] = [1,2,3,4,5,6,7,8,9]
+for ordinale in numeri_ordinali:
+    if ordinale == numeri_ordinali[0]:
+        print(f"{ordinale}st")
+    elif ordinale == numeri_ordinali[1]:
+        print(f"{ordinale}nd")
+    elif ordinale == numeri_ordinali[2]:
+        print(f"{ordinale}rd")
+    else:
+        print(f"{ordinale}th")
+
+
+
+    
+    
+
+
+    
+    
