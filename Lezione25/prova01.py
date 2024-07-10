8f98c222e3dcab1b34d34cb9e17f5ee86979fbca
Vincecasu <vincenzocasuccio77@gmail.com>
2024-07-10 17:20:31 +0200

    Multiprocessing

---
+++ Lezione25/prova01.py
@@ -0,0 +1,44 @@
+# from multiprocessing import Process
+# import time
+
+# def bubble_sort_v2():
+
+#     from random import randint
+    
+#     x = [randint(0, 10000) for _ in range(50000)]
+    
+#     ho_fatto_swap: bool = True
+#     for i in range(len(x)):
+#         for j in range(len(x) - i - 1):
+#             if x[j] > x[j+1]:
+#                 # swap(x[j], x[j+1])
+#                 ho_fatto_swap = False
+#                 temp: int = x[j]
+#                 x[j] = x[j+1]
+#                 x[j+1] = temp
+#         if ho_fatto_swap:
+#             break
+
+
+from multiprocessing import Process
+import time
+
+def sleep():
+    
+    print(f"Sono nella funzione")
+    time.sleep(60)
+    print(f"Sto uscendo dalla funzione")
+    
+if __name__ == "__main__":
+    
+    tic: float = time.time()
+    t1 = Process(target=sleep)
+    t2 = Process(target=sleep)
+    t1.start()
+    t2.start()
+    #t1.join() # ricorda di vedere con o senza join
+    #t2.join() # ricorda di vedere con o senza join
+    toc: float = time.time()
+    time_elapsed: float = toc - tic 
+
+    print(f"{time_elapsed=}")
